4c07c83fac37f02d554187f9c94c3ebb5e15520e
Aleksey Mogilnikov <tuxrps@gmail.com>
2023-08-21 01:34:32 +0400

    LAB-0: add base image

---
+++ base_image/packer/scripts/stage-1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yum install -y tmux mc vim
+
+# Remove temporary files
+rm -rf /tmp/*
+rm  -f /var/log/wtmp /var/log/btmp
+rm -rf /var/cache/* /usr/share/doc/*
+rm -rf /var/cache/yum
+rm -rf /vagrant/home/*.iso
+rm  -f ~/.bash_history
+history -c
+rm -rf /run/log/journal/*
+sync
